bb73ff263f3b72eedab6bb4d81bec9cf277d87c8
“sammrguez“ <sammrguez@gmail.com>
2024-08-06 13:36:48 -0600

    designed new buttons

--- src/components/ConfirmationDialog.jsx
+++ src/components/ConfirmationDialog.jsx
@@ -4,9 +4,16 @@ function ConfirmationDialog() {
   return (
     <div className='confirmationDialog'>
       <div className='confirmationDialog__container'>
-        <h3 className='confirmationDialog__header'>ConfirmationDialog</h3>
-        <div> aceptar </div>
-        <div> cancelar </div>
+        <h3 className='confirmationDialog__title'>¿Deseas eliminar este producto de tu carrito?</h3>
+        <div className='confirmationDialog__buttonsBox'>
+        <button className='button button_type_accept'>
+          <h3 className='button__text'>Aceptar</h3>
+        </button>
+        <button className='button button_type_cancel'> 
+           <h3 className='button__text'>Cancelar </h3>
+        </button>
+        </div>
+       
       </div>
     </div>
   );
