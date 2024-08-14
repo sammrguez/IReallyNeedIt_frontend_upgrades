80aa4775572f38e910a56dadbdb7aead653648ae
“sammrguez“ <sammrguez@gmail.com>
2024-08-13 21:29:28 -0600

    buttons organized

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import texture from '../images/textura_IRNI.jpg';
-import CartItem from './CartItem';
-import InfoTooltip from './InfoTooltip';
-import { CartContext } from '../contexts/CartContext';
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import texture from "../images/textura_IRNI.jpg";
+import CartItem from "./CartItem";
+import InfoTooltip from "./InfoTooltip";
+import { CartContext } from "../contexts/CartContext";
 
 function Cart({
   onAddClick,
@@ -26,25 +26,25 @@ function Cart({
       if (cart.length === 0) {
         setShouldBeInfoOpen(true);
       } else {
-        navigate('/pago');
+        navigate("/pago");
       }
     } else {
       if (cart.length === 0) {
         setShouldBeInfoOpen(true);
       } else {
         onOpenRegister();
-        navigate('/registro');
+        navigate("/registro");
       }
     }
   }
 
   return (
     <>
-      <section className='cart'>
-        <img className='decoration-band' src={texture} />
-        <div className='cart__container'>
-          <h2 className='cart__header'>Carrito de compras</h2>
-          <ul className='cart__added-items'>
+      <section className="cart">
+        <img className="decoration-band" src={texture} />
+        <div className="cart__container">
+          <h2 className="cart__header">Carrito de compras</h2>
+          <ul className="cart__added-items">
             {cart.map((item, index) => {
               return (
                 <CartItem
@@ -60,10 +60,10 @@ function Cart({
               );
             })}
           </ul>
-          <div className='cart__total-container'>
-            <div className='cart__total-data'>
-              <h3 className='cart__total-header'>Total</h3>
-              <p className='cart__amount'>
+          <div className="cart__total-container">
+            <div className="cart__total-data">
+              <h3 className="cart__total-header">Total</h3>
+              <p className="cart__amount">
                 {`$ ${cart.reduce(
                   (total, item) => total + item.price * item.quantity,
                   0
@@ -71,16 +71,16 @@ function Cart({
               </p>
             </div>
 
-            <button className='button button_type_pay' onClick={handlecart}>
-              <h3 className='button__text'>Continuar</h3>
+            <button className="button button_type_shop" onClick={handlecart}>
+              <h3 className="button__text">Continuar</h3>
             </button>
           </div>
         </div>
       </section>
       <InfoTooltip
-        header={'¡Tu carrito está vacío!'}
+        header={"¡Tu carrito está vacío!"}
         messagge={
-          'Parece que tu carrito está un poco solitario ¡Es hora de llenarlo con cosas increíbles! '
+          "Parece que tu carrito está un poco solitario ¡Es hora de llenarlo con cosas increíbles! "
         }
         shouldBeInfoOpen={shouldBeInfoOpen}
         onClose={onClose}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import PopupForm from './PopupForm';
-import addressIcon from '../images/address.png';
-import logOutIcon from '../images/cerrar-sesion.png';
-import IRNIstickerGreen from '../images/stickers_IRNI_green.png';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserContext } from '../contexts/UserContext';
+import React, { useContext } from "react";
+import PopupForm from "./PopupForm";
+import addressIcon from "../images/address.png";
+import logOutIcon from "../images/cerrar-sesion.png";
+import IRNIstickerGreen from "../images/stickers_IRNI_green.png";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
 
 function Profile({ isOpen, onClose, onLogOut, trackId }) {
   const navigate = useNavigate();
@@ -13,44 +13,44 @@ function Profile({ isOpen, onClose, onLogOut, trackId }) {
   function handleLogOut(evt) {
     evt.preventDefault();
     onLogOut();
-    navigate('/');
+    navigate("/");
   }
   function handleAddress(evt) {
     evt.preventDefault();
-    navigate('/pago');
+    navigate("/pago");
   }
 
   return (
-    <section className='profile'>
+    <section className="profile">
       <PopupForm
-        header={user ? user.name : 'comprador'}
+        header={user ? user.name : "comprador"}
         isOpen={isOpen}
         onClose={onClose}
         logo={IRNIstickerGreen}
-        type={'profile'}
+        type={"profile"}
       >
         {trackId && (
-          <h3 className='form__header_type_profile'>{`Compra en proceso con orden de compra: ${trackId}`}</h3>
+          <h3 className="form__header_type_profile">{`Compra en proceso con orden de compra: ${trackId}`}</h3>
         )}
-        <button className='button button_type_profile' onClick={handleAddress}>
-          <img
-            className='button__icon'
-            src={addressIcon}
-            alt='simbolo de mail'
-          />
-          <h3 className='button__text button__text_type_profile'>
+        <button className="button button_type_profile" onClick={handleAddress}>
+          <h3 className="button__text button__text_type_profile">
             Direccion del usuario
           </h3>
-        </button>
-        <button className='button button_type_profile' onClick={handleLogOut}>
           <img
-            className='button__icon'
-            src={logOutIcon}
-            alt='simbolo de mail'
+            className="button__icon"
+            src={addressIcon}
+            alt="simbolo de mail"
           />
-          <h3 className='button__text button__text_type_profile'>
+        </button>
+        <button className="button button_type_profile" onClick={handleLogOut}>
+          <h3 className="button__text button__text_type_profile">
             Cerrar sesión
           </h3>
+          <img
+            className="button__icon"
+            src={logOutIcon}
+            alt="simbolo de mail"
+          />
         </button>
       </PopupForm>
     </section>
