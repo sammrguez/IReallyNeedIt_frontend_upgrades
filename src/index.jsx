67071e8b30722824524c2a579c9adf599408508b
“sammrguez“ <sammrguez@gmail.com>
2024-09-26 12:48:20 -0600

    latest updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-//import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+import "./index.css";
+import App from "./components/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
